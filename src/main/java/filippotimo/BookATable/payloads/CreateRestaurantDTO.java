68c07d92cdb6200bd7caf130fafeeb55079203b2
Filippo-Timo <filtimo2002@gmail.com>
2026-03-04 12:39:21 +0100

    Adds CreateRestaurantDTO

---
+++ src/main/java/filippotimo/BookATable/payloads/CreateRestaurantDTO.java
@@ -0,0 +1,27 @@
+package filippotimo.BookATable.payloads;
+
+import filippotimo.BookATable.entities.RestaurantType;
+import jakarta.validation.constraints.*;
+
+public record CreateRestaurantDTO(
+        @NotBlank(message = "City is required")
+        @Size(min = 2, max = 30, message = "City must contain between 2 and 30 characters")
+        String city,
+        @NotNull(message = "Restaurant type is required")
+        RestaurantType restaurantType,
+        @NotNull(message = "Max seats is required")
+        @Min(value = 1, message = "Max seats must be at least 1")
+        Integer maxSeats,
+        @Size(max = 500, message = "Description cannot exceed 500 characters")
+        String description,
+        @NotNull(message = "Available seats indoor is required")
+        @Min(value = 0, message = "Available seats indoor cannot be negative")
+        Integer availableSeatsIndoor,
+        @NotNull(message = "Available seats outdoor is required")
+        @Min(value = 0, message = "Available seats outdoor cannot be negative")
+        Integer availableSeatsOutdoor,
+        @NotBlank(message = "Phone is required")
+        @Pattern(regexp = "^\\+?[0-9]{8,15}$", message = "Invalid phone number")
+        String phone
+) {
+}
