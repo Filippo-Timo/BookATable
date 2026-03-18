5a5a523c3b021f49da75fabda6ca226ff45d2ade
Filippo-Timo <filtimo2002@gmail.com>
2026-03-18 14:40:19 +0100

    Changes some errors messages

--- src/main/java/filippotimo/BookATable/payloads/reviewDTOs/UpdateReviewDTO.java
+++ src/main/java/filippotimo/BookATable/payloads/reviewDTOs/UpdateReviewDTO.java
@@ -8,8 +8,8 @@ public record UpdateReviewDTO(
         @Max(value = 5, message = "Rating cannot exceed 5")
         Integer rating,
 
-        @NotBlank(message = "Comment is required")
-        @Size(min = 10, max = 500, message = "Comment must be between 10 and 1000 characters")
+        @NotBlank(message = "Comment cannot be empty")
+        @Size(min = 10, max = 500, message = "Comment must be between 10 and 500 characters")
         String comment
 ) {
 }
